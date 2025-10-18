124892ee0c903e6a2b3bf98f11c254f7e946d2f7
hasanbozkus1 <45519841+hasanbozkus1@users.noreply.github.com>
2025-10-18 23:58:28 +0100

    Update NeroPay Payment Acquirer manifest
    Added images for the NeroPay Payment Acquirer.

--- neropay_payment/__manifest__.py
+++ neropay_payment/__manifest__.py
@@ -1,9 +1,9 @@
 {
-    'name': 'Neropay Payment Acquirer',
+    'name': 'NeroPay Payment Acquirer',
     'version': '1.0',
     'category': 'Accounting/Payment Acquirers',
-    'summary': 'Accept online payments securely with Neropay',
-    'author': 'Neropay Ltd, Stockport UK',
+    'summary': 'Accept online payments securely with NeroPay',
+    'author': 'NeroPay, Stockport UK',
     'website': 'https://neropay.app',
     'license': 'LGPL-3',
     'support': 'support@neropay.app',
@@ -11,6 +11,10 @@
     'data': [
         'data/neropay_data.xml',
         'views/payment_neropay_templates.xml',
+    ],
+    'images': [
+        'static/description/icon.png',
+        'static/description/cover.png'
     ],
     'installable': True,
     'application': False,
