4d517e7de8d95b469fafa21b95d58362ea834bb1
hasanbozkus1 <45519841+hasanbozkus1@users.noreply.github.com>
2025-10-18 23:55:17 +0100

    Add files via upload

---
+++ neropay_payment/README.md
@@ -0,0 +1,12 @@
+﻿# Neropay Payment Gateway for Odoo
+Accept secure payments with Neropay inside Odoo.
+
+## Features
+- Sandbox and Live mode
+- Automatic IPN handling
+- Supported currencies: GBP, EUR, IDR
+
+## Support
+📧 support@neropay.app  
+📞 +44 333 049 4380  
+🏢 Stockport, UK
---
+++ neropay_payment/__manifest__.py
@@ -0,0 +1,17 @@
+﻿{
+    'name': 'Neropay Payment Acquirer',
+    'version': '1.0',
+    'category': 'Accounting/Payment Acquirers',
+    'summary': 'Accept online payments securely with Neropay',
+    'author': 'Neropay Ltd, Stockport UK',
+    'website': 'https://neropay.app',
+    'license': 'LGPL-3',
+    'support': 'support@neropay.app',
+    'depends': ['payment'],
+    'data': [
+        'data/neropay_data.xml',
+        'views/payment_neropay_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
